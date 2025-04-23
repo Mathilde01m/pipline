5dd043d45a7261a9b4cb7c70d0c235d4c6d83513
Mathilde01m <91540529+Mathilde01m@users.noreply.github.com>
2025-04-23 13:34:34 +0200

    Delete pipline directory
    Fail de add au niveau de la racine

--- pipline/data-pipeline/insert.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import requests
-
-df = pd.read_csv("iris 1.csv")
-data = df.head(2).to_dict(orient="records")
-
-for row in data:
-    response = requests.post("http://localhost:8000/insert", json=row)
-    print(response.status_code, response.json())
